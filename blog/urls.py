a1a97a535c7908d02b0dc939053855b7853b8051
elo <eloisemecozzi@hotmail.fr>
2017-06-23 16:22:51 +0200

    chgt arborescence

--- blog/urls.py
+++ blog/urls.py
@@ -4,17 +4,10 @@
 from django.conf.urls import url
 from . import views
 
-# Url qui renvoie la date actuelle à l'utilisateur :
-urlspatterns = [
+
+ urlpatterns = [
+     url(r'^accueil$', views.home), # Accueil du blog
+     url(r'^article/(\d+)$', views.view_article),  # Vue d'un article
     url(r'^date$', views.date_actuelle),
     url(r'^addition/(?P<nombre1>\d+)/(?P<nombre2>\d+)/$', views.addition)
-]
-
-
-
-from blog import views
-
-urlpatterns = [
-    url(r'^accueil$', views.home), # Accueil du blog
-    url(r'^article/(\d+)$', views.view_article),  # Vue d'un article
-]
+ ]
