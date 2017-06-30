3314d949bc0f3e69a62a7d03856dc1531a6af3bc
elo <eloisemecozzi@hotmail.fr>
2017-06-30 17:03:19 +0200

    déplacement class articleAdmin de models.py vers admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -2,7 +2,19 @@
 from __future__ import unicode_literals
 from django.contrib import admin
 from .models import Categorie, Article
+
+
+
+
+from django.contrib import admin
+from blog.models import Categorie, Article
+
+class ArticleAdmin(admin.ModelAdmin):
+   list_display   = ('titre', 'auteur', 'date')
+   list_filter    = ('auteur','categorie',)
+   date_hierarchy = 'date'
+   ordering       = ('date', )
+   search_fields  = ('titre', 'contenu')
+
 admin.site.register(Categorie)
 admin.site.register(Article, ArticleAdmin)
-
-# Register your models here.
--- blog/models.py
+++ blog/models.py
@@ -23,20 +23,6 @@ class Article(models.Model):
         return self.titre
 
 
-from django.contrib import admin
-from blog.models import Categorie, Article
-
-class ArticleAdmin(admin.ModelAdmin):
-   list_display   = ('titre', 'auteur', 'date')
-   list_filter    = ('auteur','categorie',)
-   date_hierarchy = 'date'
-   ordering       = ('date', )
-   search_fields  = ('titre', 'contenu')
-
-admin.site.register(Categorie)
-admin.site.register(Article, ArticleAdmin)
-
-
 
 class Categorie(models.Model):
     nom = models.CharField(max_length=30)
