623d43b49e5204118acaed125ba9658fdebcd4fc
elo <eloisemecozzi@hotmail.fr>
2017-06-30 15:08:49 +0200

    ajout class articleAdmin dans models.py et admin.site dans admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.contrib import admin
+from .models import Categorie, Article
+admin.site.register(Categorie)
+admin.site.register(Article, ArticleAdmin)
 
 # Register your models here.
--- blog/models.py
+++ blog/models.py
@@ -12,6 +12,8 @@ class Article(models.Model):
     date = models.DateTimeField(auto_now_add=True, auto_now=False,
                                 verbose_name="Date de parution")
 
+    categorie = models.ForeignKey('Categorie')
+
     def __str__(self):
         """
         Cette méthode que nous définirons dans tous les modèles
@@ -19,3 +21,23 @@ class Article(models.Model):
         nous traiterons plus tard et dans l'administration
         """
         return self.titre
+
+from django.contrib import admin
+from blog.models import Categorie, Article
+
+class ArticleAdmin(admin.ModelAdmin):
+   list_display   = ('titre', 'auteur', 'date')
+   list_filter    = ('auteur','categorie',)
+   date_hierarchy = 'date'
+   ordering       = ('date', )
+   search_fields  = ('titre', 'contenu')
+
+admin.site.register(Categorie)
+admin.site.register(Article, ArticleAdmin)
+
+
+class Categorie(models.Model):
+    nom = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.nom
